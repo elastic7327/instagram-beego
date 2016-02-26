566e93258a7f859169c7b6c3b5b773b17bab4eab
Tri Truong <tri.itvn@gmail.com>
2016-02-26 11:31:37 +0700

    Init database connection when run main

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	_ "github.com/instagram-beego/routers"
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/instagram-beego/database"
+	_ "github.com/instagram-beego/routers"
 )
 
 func main() {
+	err := database.InitDBConnection()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	database.RegisterModels()
 	beego.Run()
 }
-
