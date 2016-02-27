263672b8d233d5aa676933c7dc3b1e38119a4994
Tri Truong <tri.itvn@gmail.com>
2016-02-27 11:50:02 +0700

    Update create user

--- controllers/user.go
+++ controllers/user.go
@@ -5,6 +5,7 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/instagram-beego/models"
 	"github.com/instagram-beego/parser/request"
+	"github.com/instagram-beego/parser/response"
 	"github.com/instagram-beego/repository"
 )
 
@@ -22,9 +23,22 @@ func (this *UserController) CreateUser() {
 		Email:       registerFormRequest.Email,
 		Password:    registerFormRequest.Passsword,
 	}
-	userRepository.Create(&user)
 
-	this.Ctx.Output.SetStatus(400)
+	_, err := userRepository.Create(&user)
+
+	if err != nil {
+		this.Ctx.Output.SetStatus(400)
+		this.Data["json"] = &response.ErrorResponse{
+			ExitCode: 1,
+			Message:  err.Error(),
+		}
+	} else {
+		this.Data["json"] = &user
+	}
+
+	this.ServeJSON()
+}
+
 
 	this.Data["json"] = &user
 	this.ServeJSON()
