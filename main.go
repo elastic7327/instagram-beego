91ce4a169be313bf2c7ea21b19f1e8baf9d1b898
Tri Truong <tri.itvn@gmail.com>
2016-03-01 14:51:45 +0700

    Main: load .env and test S3 when run app

--- main.go
+++ main.go
@@ -5,14 +5,21 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/instagram-beego/database"
 	_ "github.com/instagram-beego/routers"
+	"github.com/instagram-beego/services"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
+	services.TestS3()
+
 	err := database.InitDBConnection()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 	database.RegisterModels()
 	beego.Run()
 }
